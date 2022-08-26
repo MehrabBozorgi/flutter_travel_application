70ab7a937e37faed77212cc8fe7ba3ca8cfbc261
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2022-08-26 11:53:52 +0430

    travel_application

---
+++ lib/tools/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+final darkColor1 = Colors.grey.shade900;
+const darkColor2 = Colors.black54;
+final greyColor = Colors.grey.shade400;
+const whiteColor = Colors.white;
